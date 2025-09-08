3c35fad63708cd7b6e2b67a91177e46babada649
s870012 <s8700122000@gmail.com>
2025-09-08 17:03:19 +0800

    login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,47 @@
 import { NavLink } from 'react-router'
 
 import Logo from '../assets/images/Logo.png'
-import singIn from '../assets/images/sign-in.png'
+import signIn from '../assets/images/sign-in.png'
+import singInDone from '../assets/images/sign-in-done.png'
+import { useState } from 'react'
 
 function Header () {
-  return (<>
+    const [isLogin, setIsLogin] = useState(false)
+    const handleLogin = (e) => {
+        e.preventDefault()
+        setIsLogin(true)
+    }
+
+    return (<>
     <nav className="navbar navbar-expand-xxxl nav-border nav-position bg-white rounded-bottom-3 py-0 nav-width zindex-1000">
-      <div className="d-flex flex-column w-100">
-          <div className="d-flex justify-content-between">
-              <h1 className="mx-xxxl-auto pt-xxxl-11 pb-xxxl-6"><NavLink className="navbar-brand m-0" href="index.html"><img src={Logo} alt="logo" className="logo"/></NavLink></h1>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
-                  <span className="navbar-toggler-icon"></span>
-              </button>
-          </div>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav flex-xxxl-column text-center fw-medium w-100">
-                  <li>
-                      <form action="#" className="input-group w-100">
-                          <label htmlFor="search" className="input-group-text pe-0 bg-white border-search border-start-0 rounded-0"><i className="bi bi-search text-gray-40"></i></label>
-                          <input type="text" id="search" placeholder="搜尋" className="form-control border-input border-end-0 rounded-0 ps-2"/>
-                      </form>
-                  </li>
-                  <li className="nav-item"><a className="nav-link active text-gray-60 pt-6"  href="#">精選文章</a></li>
-                  <NavLink to="/course" className="nav-item nav-link text-gray-60" href="mainCourse.html">課程專區</NavLink>
-                  <NavLink to="/profile" className="nav-item nav-link text-gray-60">我的課程</NavLink>
-                  <NavLink to="/about" className="nav-item nav-link text-gray-60">關於我們</NavLink>
-                  <li><a href="#"><img src={singIn} alt="sign in" className="clockIN pt-2 pb-6"/></a></li>
-                  <li className="signIN d-flex align-items-center">
-                      <a className="btn btn-primary nav-btn-hover btn-border-top rounded-0 rounded-bottom-3 w-100 fs-5 fw-medium" href="#">登入 / 註冊</a>
-                  </li>
-              </ul>
-          </div>
-      </div>
+        <div className="d-flex flex-column w-100">
+            <div className="d-flex justify-content-between">
+                <h1 className="mx-xxxl-auto pt-xxxl-11 pb-xxxl-6"><NavLink className="navbar-brand m-0" href="index.html"><img src={Logo} alt="logo" className="logo"/></NavLink></h1>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav flex-xxxl-column text-center fw-medium w-100">
+                    <li>
+                        <form action="#" className="input-group w-100">
+                            <label htmlFor="search" className="input-group-text pe-0 bg-white border-search border-start-0 rounded-0"><i className="bi bi-search text-gray-40"></i></label>
+                            <input type="text" id="search" placeholder="搜尋" className="form-control border-input border-end-0 rounded-0 ps-2"/>
+                        </form>
+                    </li>
+                    <li className="nav-item"><a className="nav-link active text-gray-60 pt-6"  href="#">精選文章</a></li>
+                    <NavLink to="/course" className="nav-item nav-link text-gray-60" href="mainCourse.html">課程專區</NavLink>
+                    <NavLink to="/profile" className="nav-item nav-link text-gray-60">我的課程</NavLink>
+                    <NavLink to="/about" className="nav-item nav-link text-gray-60">關於我們</NavLink>
+                    <li><a href="#" onClick={(e) => handleLogin(e)}><img src={`${isLogin ? singInDone : signIn}`} alt="sign in" className="clockIN pt-2 pb-6"/></a></li>
+                    <li className="signIN d-flex align-items-center">
+                        <a className="btn btn-primary nav-btn-hover btn-border-top rounded-0 rounded-bottom-3 w-100 fs-5 fw-medium" href="#">登入 / 註冊</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
-  </>)
+    </>)
 }
 
 export default Header
